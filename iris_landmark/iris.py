2804748425e4308a05ae99e20a0b1cf9a42d8c71
sunway <sunwayforever@gmail.com>
2020-12-16 14:04:54 +0800

    nil

--- iris_landmark/iris.py
+++ iris_landmark/iris.py
@@ -39,41 +39,54 @@ class Iris(object):
         iris = self.iris_cropper(img)
         if iris is None:
             return None, None
-        # TODO: left eye only
-        img, offset_x, offset_y, offset_x2, offset_y2 = iris[0]
-        # cv2.rectangle(img_orig, (offset_x, offset_y), (offset_x2, offset_y2), (255, 0, 0), 2)
-        # cv2.imshow("", img_orig)
-        # cv2.waitKey(0)
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        input_data = cv2.resize(img_rgb, (self.input_width, self.input_height)).astype(
-            np.float32
-        )
-        input_data = input_data / 255.0
-        input_data = np.expand_dims(input_data, axis=0)
+        eye_surfaces = []
+        iris_surfaces = []
+        for index, (img, offset_x, offset_y, _, _) in enumerate(iris):
+            # right eye need to be horizontal flipped
+            if index == 1:
+                img = cv2.flip(img, 1)
 
-        self.interpreter.set_tensor(self.input_details[0]["index"], input_data)
-        self.interpreter.invoke()
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            input_data = cv2.resize(
+                img_rgb, (self.input_width, self.input_height)
+            ).astype(np.float32)
+            input_data = input_data / 255.0
+            input_data = np.expand_dims(input_data, axis=0)
 
-        # 71x3
-        eye_surface = self.interpreter.get_tensor(self.output_details[0]["index"])
-        # 5x3
-        #           2
-        #
-        #    3      0       1
-        #
-        #           4
-        iris_surface = self.interpreter.get_tensor(self.output_details[1]["index"])
-        eye_surface, iris_surface = (
-            np.reshape(eye_surface, (-1, 3)),
-            np.reshape(iris_surface, (-1, 3)),
-        )
+            self.interpreter.set_tensor(self.input_details[0]["index"], input_data)
+            self.interpreter.invoke()
 
-        img_height, img_width = img.shape[0], img.shape[1]
-        eye_surface[:, 0] = (eye_surface[:, 0] / IMG_WIDTH * img_width) + offset_x
-        eye_surface[:, 1] = (eye_surface[:, 1] / IMG_HEIGHT * img_height) + offset_y
-        iris_surface[:, 0] = (iris_surface[:, 0] / IMG_WIDTH * img_width) + offset_x
-        iris_surface[:, 1] = (iris_surface[:, 1] / IMG_HEIGHT * img_height) + offset_y
-        return eye_surface, iris_surface
+            # 71x3
+            eye_surface = self.interpreter.get_tensor(self.output_details[0]["index"])
+            # 5x3
+            #           2
+            #
+            #    3      0       1
+            #
+            #           4
+            iris_surface = self.interpreter.get_tensor(self.output_details[1]["index"])
+            eye_surface, iris_surface = (
+                np.reshape(eye_surface, (-1, 3)),
+                np.reshape(iris_surface, (-1, 3)),
+            )
+
+            # # right eye was horizontal flipped
+            if index == 1:
+                eye_surface[:, 0] = IMG_WIDTH - eye_surface[:, 0]
+                iris_surface[:, 0] = IMG_WIDTH - iris_surface[:, 0]
+
+            img_height, img_width = img.shape[0], img.shape[1]
+            eye_surface[:, 0] = (eye_surface[:, 0] / IMG_WIDTH * img_width) + offset_x
+            eye_surface[:, 1] = (eye_surface[:, 1] / IMG_HEIGHT * img_height) + offset_y
+            iris_surface[:, 0] = (iris_surface[:, 0] / IMG_WIDTH * img_width) + offset_x
+            iris_surface[:, 1] = (
+                iris_surface[:, 1] / IMG_HEIGHT * img_height
+            ) + offset_y
+
+            eye_surfaces.append(eye_surface)
+            iris_surfaces.append(iris_surface)
+
+        return eye_surfaces, iris_surfaces
 
 
 def remap(x, lo, hi, scale):
@@ -83,35 +96,17 @@ def remap(x, lo, hi, scale):
 eye_landmark_connections = [0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 14, 13, 12, 11, 10, 9]
 
 
-def annotate_image(img, eye_surface, iris_surface):
-    # for x, y, _ in eye_surface:
-    #     cv2.circle(img, (x, y), color=(0, 0, 255), radius=1)
-    for x, y, _ in iris_surface:
-        cv2.circle(img, (x, y), 1, color=(0, 255, 0))
-    # eye_surface = eye_surface[:, :2]
-    # blank_img = np.zeros(shape=(5120, 5120, 3), dtype=np.uint8)
-    # for index, (a, b) in enumerate(eye_surface):
-    #     cv2.putText(
-    #         blank_img,
-    #         f"{index}",
-    #         (int(a) * 10, int(b) * 10),
-    #         cv2.FONT_HERSHEY_SIMPLEX,
-    #         0.6,
-    #         (0, 0, 255),
-    #         2,
-    #     )
-    #     cv2.circle(blank_img, (int(a) * 10, int(b) * 10), 1, color=(0, 255, 0))
-    # cv2.imshow("", blank_img)
-    # while True:
-    #     if cv2.waitKey(1) & 0xFF == ord("q"):
-    #         break
-    for a, b in zip(eye_landmark_connections[::2], eye_landmark_connections[1::2]):
-        cv2.line(
-            img,
-            (eye_surface[a][0], eye_surface[a][1]),
-            (eye_surface[b][0], eye_surface[b][1]),
-            color=(0, 0, 255),
-        )
+def annotate_image(img, eye_surfaces, iris_surfaces):
+    for eye_surface, iris_surface in zip(eye_surfaces, iris_surfaces):
+        for x, y, _ in iris_surface:
+            cv2.circle(img, (x, y), 1, color=(0, 255, 0))
+        for a, b in zip(eye_landmark_connections[::2], eye_landmark_connections[1::2]):
+            cv2.line(
+                img,
+                (eye_surface[a][0], eye_surface[a][1]),
+                (eye_surface[b][0], eye_surface[b][1]),
+                color=(0, 0, 255),
+            )
     return img
 
 
@@ -122,8 +117,8 @@ def sigmoid(x):
 def iris_image(img):
     iris = Iris()
     img = cv2.imread(img)
-    eye_surface, iris_surface = iris(img)
-    img = annotate_image(img, eye_surface, iris_surface)
+    eye_surfaces, iris_surfaces = iris(img)
+    img = annotate_image(img, eye_surfaces, iris_surfaces)
     cv2.imshow("", img)
     while cv2.waitKey(1) & 0xFF != ord("q"):
         pass
